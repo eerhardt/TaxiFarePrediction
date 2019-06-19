1401a602894ce4a1d9fb0fc4c9fa210ea2458c47
Eric Erhardt <eric.erhardt@microsoft.com>
2019-06-19 14:38:04 -0500

    fixup project.
    use packagereference
    hardcode x64 for AnyCPU config

--- TaxiFarePrediction.Interpretability/Form1.cs
+++ TaxiFarePrediction.Interpretability/Form1.cs
@@ -76,7 +76,7 @@ namespace TaxiFareRegression.Interpretability
         }
         internal static class GetTaxiFare
         {
-            private static string BaseRelativePath = @"../../../../TaxiFarePrediction";
+            private static string BaseRelativePath = @"../../../TaxiFarePrediction";
             private static string BaseDataPath = Path.Combine(Path.GetFullPath(BaseRelativePath), "inputs");
             private static string TestDataPath = Path.Combine(BaseDataPath, "taxi-fare-test.csv");
             private static string ModelPath = Path.Combine(BaseRelativePath, "outputs", "TaxiFareModel.zip");
